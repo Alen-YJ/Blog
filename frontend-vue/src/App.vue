e64873c4a8556f308aa5ac1a6cd211c91549af08
yijing <yijing5893@dingtalk.com>
2020-09-08 18:39:43 +0800

    基础头部组件

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,6 +1,9 @@
 <template>
     <v-app class='app'>
         <Header></Header>
+        <v-main>
+            <router-view></router-view>
+        </v-main>
     </v-app>
 </template>
 
@@ -22,4 +25,7 @@
 </script>
 
 <style lang="scss" scoped>
+    .app{
+        font-family: tahoma,"microsoft yahei";
+    }
 </style>
--- frontend-vue/src/components/Header.vue
+++ frontend-vue/src/components/Header.vue
@@ -5,12 +5,10 @@
                 <v-img src='../assets/logo.png' :contain='true' max-height='100%'/>
             </a>
         </div>
-        <div>
-            <v-spacer></v-spacer>
-            <!-- <v-btn icon>
-                <icon>arrow-compress-down</icon>
-            </v-btn> -->
-            <route-link to='/'>首页</route-link>
+        <div class='nav-list'>
+            <router-link to='/' exact>首页</router-link>
+            <router-link to='/article'>文章列表</router-link>
+            <router-link to='/about'>关于</router-link>
         </div>
     </v-app-bar>
 </template>
@@ -22,8 +20,29 @@
 </script>
 
 <style lang="scss" scoped>
+    @import "../styles/variables";
     .logo{
         padding-left:20px;
-        width:150px;
+        width:130px;
+    }
+    ::v-deep .v-toolbar__content{
+        justify-content: space-between;
+        a{
+            color:$secondary;
+            margin-right:10px;
+            text-decoration: $link-decoration;
+            padding:$menu-item-padding-y $menu-item-padding-x;
+            &:hover{
+                color:$link-hover-color;
+            }
+            &.router-link-active{
+                color:white;
+                background-color:$menu-item-active-color;
+                border-color:$border-color;
+                border-width:$menu-item-active-border-width;
+                box-shadow:$menu-box-shadow;
+                transition: $menu-transition;
+            }
+        }
     }
 </style>
