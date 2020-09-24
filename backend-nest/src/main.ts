03e5977a6a9cdc1b137394b2c2c0c5fd384e6e3c
yijing <yijing5893@dingtalk.com>
2020-09-24 16:44:34 +0800

    拦截器异常处理

--- backend-nest/src/main.ts
+++ backend-nest/src/main.ts
@@ -2,13 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express'
 import LoggerMiddleware from './middleware/logger.middleware'
+import { TransformInterceptor } from './interceptor/transform.interceptor'
+import { HttpExceptionFilter } from './filter/http-exception.filter'
+import { AllExceptionFilter } from './filter/any-exception.filter'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(express.json())
-  app.use(express.urlencoded({ extended: true }))
-
-  app.use( LoggerMiddleware.use )
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
+    //日志
+    app.use( LoggerMiddleware.use )
+    //拦截器打印出参
+    app.useGlobalInterceptors(new TransformInterceptor())
+    //过滤器 处理异常
+    app.useGlobalFilters(new AllExceptionFilter())
+    app.useGlobalFilters(new HttpExceptionFilter())
+    await app.listen(3000);
 }
 bootstrap();
