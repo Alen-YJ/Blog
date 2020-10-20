374d0fd91a42c24dd7512d07fad9d030aa1ea93a
yijing <yijing5893@dingtalk.com>
2020-10-20 17:17:49 +0800

    开启cos

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -8,16 +8,16 @@ import { ArticleService } from './logical/article/article.service';
 import { CategoryService } from './logical/category/category.service';
 import { CategoryController } from './logical/category/category.controller';
 import { ArticleController } from './logical/article/article.controller';
-// import { CosService } from './logical/cos/cos.service';
-// import { CosController } from './logical/cos/cos.controller';
+import { CosService } from './logical/cos/cos.service';
+import { CosController } from './logical/cos/cos.controller';
 
 @Module({
   imports: [UserModule, AuthModule],
   controllers: [AppController, UserController, CategoryController, ArticleController, 
-    // CosController
+    CosController
   ],
   providers: [AppService, ArticleService, CategoryService, 
-    // CosService
+    CosService
   ],
 })
 export class AppModule {}
